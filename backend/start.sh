9278906549f50228d2b9fb1adbb83aaa169cede4
Meshach Zachariah <MZacx54@users.noreply.github.com>
2026-01-11 03:54:03 +0100

    Fix 502: Force update Dockerfile and start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -6,4 +6,5 @@ python manage.py migrate
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 echo "Starting Gunicorn..."
+PORT=${PORT:-8000}
 gunicorn smartbiz_backend.wsgi:application --bind 0.0.0.0:$PORT --log-file -
